aa4ed0775fa4a500ce574507df114870277663cd
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-06-19 18:09:11 +0530

    added value logic

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -4,8 +4,20 @@ export default function Grid() {
     const [vals, setVals] = useState(new Array(9).fill(new Array(9).fill("")));
 
     function putData(ind1, ind2, key) {
-        setVals(()=>{vals[ind1][ind2] = isNaN(key)? "": key; return vals})
-        console.log(isNaN(key))
+        setVals(
+            vals.map((val, i) => {
+                return val.map((v, i2) => {
+                    if (i2 === ind2 && i === ind1) {
+                        console.log(v, key);
+                        return isNaN(key)? v : key;
+                    }
+                    else return v;
+                })
+
+            }
+            )
+        )
+        console.log(vals)
     }
 
     let i = 0;
@@ -13,7 +25,7 @@ export default function Grid() {
         <div className="grid" id="grid">
             {
                 vals.map((val, ind1) => (
-                    val.map((cell, ind2) => (   
+                    val.map((cell, ind2) => (
                         <div className="cell" key={i} id={i++} style={{
                             borderTopLeftRadius: `${ind1 === 0 && ind2 === 0 ? "10px" : "0"}`,
                             borderBottomLeftRadius: `${ind1 === 8 && ind2 === 0 ? "10px" : "0"}`,
@@ -22,7 +34,7 @@ export default function Grid() {
                             borderRightWidth: `${ind2 % 3 === 2 ? "3px" : "1px"}`,
                             borderBottomWidth: `${ind1 % 3 === 2 ? "3px" : "1px"}`
                         }}
-                        ><input type="number" className="cell-input" maxLength={1} value={vals[ind1][ind2] === ""? "": vals[ind1][ind2]} onInput={e => (putData(ind1, ind2, e.nativeEvent.data))} /></div>
+                        ><input type="number" className="cell-input" maxLength={1} onInput={e => (putData(ind1, ind2, e.nativeEvent.data))} value={(vals[ind1][ind2] === "") ? "" : vals[ind1][ind2]} /></div>
                     ))
                 ))
             }
